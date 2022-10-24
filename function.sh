d193a035d8dfe1a329f346e78c6a433ef95fced5
Scott Ouellette <scottx611x@gmail.com>
2022-10-24 17:02:39 -0400

    Tweak to support Lambda read-only FS

--- function.sh
+++ function.sh
@@ -10,19 +10,21 @@ function handler () {
 
   PAYLOAD=$(echo "$EVENT_DATA" | jq -r '.payload' | cat)
 
-  cp main.tf main.tf.bak
+  # Note: this is going to get fucky w/ many invocations!
+  cp main.tf /tmp/main.tf.bak
+  cp main.tf /tmp/main.tf
 
   # Update our main.tf with event data
-  sed -i "s/\# DATA/${PAYLOAD}/" main.tf
+  sed -i "s/\# DATA/${PAYLOAD}/" /tmp/main.tf
 
   # Respond to Lambda service by echoing the received data back
-#  RESPONSE="Echoing request: '$EVENT_DATA'"
-#  echo $RESPONSE
-#
-#  terraform --version
-  terraform fmt main.tf 1> /dev/null
+  RESPONSE="Echoing request: '$EVENT_DATA'"
+  echo "$RESPONSE"
+
+  terraform --version
+  terraform fmt /tmp/main.tf 1> /dev/null
+  pushd /tmp || exit
   terraform init 1> /dev/null
   echo "local.a" | terraform console
-
-  cp main.tf.bak main.tf
+  popd
 }
