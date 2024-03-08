58cd488aeeee4a6c9ca6bbe5e47493d21bb884de
Scott Ouellette <scottx611x@gmail.com>
2024-03-08 16:03:10 -0500

    no quotes in newer terraform here

--- deployment/terraform/modules/deployment/vars.tf
+++ deployment/terraform/modules/deployment/vars.tf
@@ -1,13 +1,13 @@
 variable "ecr_repo_name" {
-  type = "string"
+  type = string
 }
 
 variable "eks_cluster_name" {
-  type = "string"
+  type = string
 }
 
 variable "ssl_cert_arn" {
-  type = "string"
+  type = string
 }
 
 variable "subdomain" {
