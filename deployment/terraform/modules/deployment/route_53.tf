53d5d19654c6fea3efb39b200e02b2d456d86913
Scott Ouellette <scottx611x@gmail.com>
2024-03-08 16:05:30 -0500

    fix zone_id reference

--- deployment/terraform/modules/deployment/route_53.tf
+++ deployment/terraform/modules/deployment/route_53.tf
@@ -4,7 +4,7 @@ data "aws_route53_zone" "hcl_playground_zone" {
 }
 
 resource "aws_route53_record" "this" {
-  zone_id = data.aws_route53_zone
+  zone_id = data.aws_route53_zone.hcl_playground_zone.zone_id
   name    = "${var.subdomain}.hcl-playground.com"
   type    = "A"
 
