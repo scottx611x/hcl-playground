25d086d0302c84b7f1646aeac922a3babdae67dc
Scott Ouellette <scottx611x@gmail.com>
2024-03-09 17:32:14 -0500

    new subdomain for each commit was annoying

--- deployment/terraform/development/main.tf
+++ deployment/terraform/development/main.tf
@@ -9,12 +9,11 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-variable "subdomain_from_env" {}
 
 module "deployment" {
   source = "../modules/deployment"
   ecr_repo_name = "hcl-playground-development"
   eks_cluster_name = "hcl-playground-development"
   ssl_cert_arn = "arn:aws:acm:us-east-1:386710959426:certificate/e4698b03-63d5-4133-a18a-7b692a41afd9"
-  subdomain = var.subdomain_from_env
+  subdomain = "development"
 }
