265275ac41bfff92df064f353f764f333e2ab2e7
Scott Ouellette <scottx611x@gmail.com>
2024-03-11 14:21:57 -0400

    add some nodes

--- deployment/terraform/modules/deployment/eks.tf
+++ deployment/terraform/modules/deployment/eks.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "this" {
   name     = var.eks_cluster_name
-  role_arn = aws_iam_role.this.arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = aws_subnet.this[*].id
@@ -12,6 +12,28 @@ resource "aws_eks_cluster" "this" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.this,
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+  ]
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "example-eks-node-group"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
+  subnet_ids      = aws_subnet.this[*].id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t4g.micro"]
+
+  capacity_type = "SPOT"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
   ]
 }
--- deployment/terraform/modules/deployment/iam.tf
+++ deployment/terraform/modules/deployment/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "this" {
+resource "aws_iam_role" "eks_cluster_role" {
   name = "eksClusterRole"
 
   assume_role_policy = jsonencode({
@@ -15,7 +15,34 @@ resource "aws_iam_role" "this" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "this" {
-  role       = aws_iam_role.this.name
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_worker_role" {
+  name = "example-eks-worker-role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
