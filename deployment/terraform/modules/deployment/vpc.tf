e68c6922de7052d15dd456e4a224f2d3d0fb51c5
Scott Ouellette <scottx611x@gmail.com>
2024-03-25 21:40:06 -0400

    map_public_ip_on_launch = true

--- deployment/terraform/modules/deployment/vpc.tf
+++ deployment/terraform/modules/deployment/vpc.tf
@@ -9,6 +9,7 @@ resource "aws_subnet" "this" {
   vpc_id     = aws_vpc.this.id
   cidr_block = count.index == 0 ? "10.0.1.0/24" : "10.0.2.0/24"
   availability_zone = count.index == 0 ? "us-east-1a" : "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "my_igw" {
