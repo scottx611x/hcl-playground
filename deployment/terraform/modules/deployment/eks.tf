e0248de7f27c7784aad6b11795fd8482741712e3
Scott Ouellette <scottx611x@gmail.com>
2024-03-26 20:52:24 -0400

    mutually exclusive when specifying a launch_template ami

--- deployment/terraform/modules/deployment/eks.tf
+++ deployment/terraform/modules/deployment/eks.tf
@@ -38,7 +38,6 @@ resource "aws_eks_node_group" "this" {
   node_group_name = "hcl-playground-development-node-group"
   node_role_arn   = aws_iam_role.eks_worker_role.arn
   subnet_ids      = aws_subnet.this[*].id
-  version         = aws_eks_cluster.this.version
 
   scaling_config {
     desired_size = 2
