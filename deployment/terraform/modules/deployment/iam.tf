ce114b400a31ae4f5130b4f04e6d82598bbee7e0
Scott Ouellette <scottx611x@gmail.com>
2024-04-01 15:54:58 -0400

    let worker nodes list ACM certs

--- deployment/terraform/modules/deployment/iam.tf
+++ deployment/terraform/modules/deployment/iam.tf
@@ -37,6 +37,30 @@ resource "aws_iam_role" "eks_worker_role" {
   })
 }
 
+resource "aws_iam_policy" "eks_worker_policy" {
+  name        = "eks-worker-policy"
+  path        = "/"
+  description = "Policy for EKS workers"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "acm:ListCertificates",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_policy_attachment" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = aws_iam_policy.eks_worker_policy.arn
+}
+
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
   role       = aws_iam_role.eks_worker_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -54,5 +78,5 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
 
 # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/823#issuecomment-633783718
 resource "aws_iam_service_linked_role" "AWSServiceRoleForAmazonEKSNodegroup" {
-aws_service_name = "eks-nodegroup.amazonaws.com"
+  aws_service_name = "eks-nodegroup.amazonaws.com"
 }
