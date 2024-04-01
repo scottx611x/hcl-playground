8e0013b0df294bcf5fcadef0afc547fa9c017f0c
Scott Ouellette <scottx611x@gmail.com>
2024-04-01 14:20:19 -0400

    Manage lb resources via terraform

--- deployment/terraform/modules/deployment/route_53.tf
+++ deployment/terraform/modules/deployment/route_53.tf
@@ -3,14 +3,14 @@ data "aws_route53_zone" "hcl_playground_zone" {
   private_zone = false
 }
 
-#resource "aws_route53_record" "this" {
-#  zone_id = data.aws_route53_zone.hcl_playground_zone.zone_id
-#  name    = "${var.subdomain}.hcl-playground.com"
-#  type    = "A"
+resource "aws_route53_record" "this" {
+  zone_id = data.aws_route53_zone.hcl_playground_zone.zone_id
+  name    = "${var.subdomain}.hcl-playground.com"
+  type    = "A"
 
-#  alias {
-#    name                   = aws_lb.this.dns_name
-#    zone_id                = aws_lb.this.zone_id
-#    evaluate_target_health = true
-#  }
-#}
+  alias {
+    name                   = aws_lb.this.dns_name
+    zone_id                = aws_lb.this.zone_id
+    evaluate_target_health = true
+  }
+}
