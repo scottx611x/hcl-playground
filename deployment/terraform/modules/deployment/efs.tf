3787eae205ab91109adf02e7ba6ac0771bbaf22f
Scott Ouellette <scottx611x@gmail.com>
2024-04-02 13:39:28 -0400

    Add mount targets in each subnet

--- deployment/terraform/modules/deployment/efs.tf
+++ deployment/terraform/modules/deployment/efs.tf
@@ -2,6 +2,12 @@ resource "aws_efs_file_system" "this" {
   creation_token = var.eks_cluster_name
 }
 
+resource "aws_efs_mount_target" "this" {
+  for_each = aws_subnet.this[*].id
+  file_system_id = aws_efs_file_system.this.id
+  subnet_id      = each.value
+}
+
 output "efs_id" {
   value = aws_efs_file_system.this.id
 }
