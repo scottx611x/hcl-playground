b94de7eef5718a2d4c5c8b1602d9c2c26c872a8d
Scott Ouellette <scottx611x@gmail.com>
2024-04-02 13:50:00 -0400

    add sgs

--- deployment/terraform/modules/deployment/efs.tf
+++ deployment/terraform/modules/deployment/efs.tf
@@ -6,6 +6,9 @@ resource "aws_efs_mount_target" "this" {
   for_each       = toset(aws_subnet.this[*].id)
   file_system_id = aws_efs_file_system.this.id
   subnet_id      = each.value
+
+  # TODO: eks made this behind the scenes
+  security_groups = ["sg-091424d543f6f0ac7"]
 }
 
 output "efs_id" {
