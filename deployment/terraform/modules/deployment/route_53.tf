2c02711f226c9e053dd7f90a24869a7d02395f06
Scott Ouellette <scottx611x@gmail.com>
2024-04-01 20:49:47 -0400

    put a terraform on it

--- deployment/terraform/modules/deployment/route_53.tf
+++ deployment/terraform/modules/deployment/route_53.tf
@@ -3,14 +3,23 @@ data "aws_route53_zone" "hcl_playground_zone" {
   private_zone = false
 }
 
-#resource "aws_route53_record" "this" {
-#  zone_id = data.aws_route53_zone.hcl_playground_zone.zone_id
-#  name    = "${var.subdomain}.hcl-playground.com"
-#  type    = "A"
-#
-#  alias {
-#    name                   = aws_lb.this.dns_name
-#    zone_id                = aws_lb.this.zone_id
-#    evaluate_target_health = true
-#  }
-#}
+# TODO: the current order of operations doesn't allow for this to be provisioned until after the cluster is up and the
+# loadbalancer controller does its magic
+data "aws_lb" "this" {
+  filter {
+    name   = "tag:elbv2.k8s.aws/cluster"
+    values = [var.eks_cluster_name]
+  }
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = "<your-hosted-zone-id>"
+  name    = "example.yourdomain.com"
+  type    = "A"
+
+  alias {
+    name                   = data.aws_lb.this.dns_name
+    zone_id                = data.aws_lb.this.zone_id
+    evaluate_target_health = true
+  }
+}
